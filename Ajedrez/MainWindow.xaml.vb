b9094f55754d44daa6aa62964d0bad5bfcf0c435
José Morente <Jos Morente>
2016-08-08 18:26:57 -0600

    Movimientos de la piezas hechas!

--- Ajedrez/MainWindow.xaml.vb
+++ Ajedrez/MainWindow.xaml.vb
@@ -12,14 +12,21 @@ Class MainWindow
     Dim _alternador As Boolean
 
     Public Sub New()
-        Dim _colorNegro As New SolidColorBrush(Colors.Transparent)
-        Dim _colorBlanco As New SolidColorBrush(Colors.Black)
 
-        _colorBlanco.Opacity = 0.9
         ' Esta llamada es exigida por el diseñador.
         InitializeComponent()
 
         ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        NewGame()
+
+    End Sub
+
+    Public Sub NewGame()
+        Dim _colorNegro As New SolidColorBrush(Colors.Transparent)
+        Dim _colorBlanco As New SolidColorBrush(Colors.Black)
+
+        _colorBlanco.Opacity = 0.9
+
         For _y As Integer = 0 To 7
             For _x As Integer = 0 To 7
                 If (_x Mod 2) Then
@@ -143,7 +150,28 @@ Class MainWindow
             Tablero.Children.Add(_reinaNegro)
             _matriz(_x, 7) = _reinaNegro
         Next
+    End Sub
 
+    Private Sub MenuItem_Click(sender As Object, e As RoutedEventArgs)
+        Dim Msg As MsgBoxResult
+        Msg = MsgBox("¿Deseas empezar nuevo juego?", vbYesNo, "Nuevo Juego")
+        If Msg = MsgBoxResult.Yes Then
+            Tablero.Children.Clear()
+            NewGame()
+        Else
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub MenuItem_Click_1(sender As Object, e As RoutedEventArgs)
+        Dim Msg As MsgBoxResult
+        Keyboard.IsKeyDown(Key.LeftCtrl + Key.S)
+        Msg = MsgBox("¿Desea cerrar la partida?", vbYesNo, "Salir")
+        If Msg = MsgBoxResult.Yes Then
+            Close()
+        Else
+            Exit Sub
+        End If
     End Sub
 
     Private Sub Tablero_MouseMove(sender As Object, e As MouseEventArgs)
@@ -151,45 +179,8 @@ Class MainWindow
         Dim _ordenadas As String = ""
         _mouseX = e.GetPosition(sender).X \ 68.75
         _mouseY = e.GetPosition(sender).Y \ 68.75
-        Select Case _mouseX
-            Case 0
-                _abcisas = "A"
-            Case 1
-                _abcisas = "B"
-            Case 2
-                _abcisas = "C"
-            Case 3
-                _abcisas = "D"
-            Case 4
-                _abcisas = "E"
-            Case 5
-                _abcisas = "F"
-            Case 6
-                _abcisas = "G"
-            Case 7
-                _abcisas = "H"
-        End Select
 
-        Select Case _mouseY
-            Case 0
-                _ordenadas = "8"
-            Case 1
-                _ordenadas = "7"
-            Case 2
-                _ordenadas = "6"
-            Case 3
-                _ordenadas = "5"
-            Case 4
-                _ordenadas = "4"
-            Case 5
-                _ordenadas = "3"
-            Case 6
-                _ordenadas = "2"
-            Case 7
-                _ordenadas = "1"
-        End Select
-
-        Informacion.Text = "Posición: " & _abcisas & "," & _ordenadas & "; Pieza: Torre; Gana: Blancas; Turno: Blancas; Tiempo: 14:53:00"
+        Informacion.Text = "Posición: " & _mouseX & "," & _mouseY & "; Pieza: Torre; Gana: Blancas; Turno: Blancas; Tiempo: 14:53:00"
     End Sub
 
     Private Sub Tablero_MouseDown(sender As Object, e As MouseButtonEventArgs)
@@ -200,48 +191,38 @@ Class MainWindow
         ElseIf Not IsNothing(_objeto) Then
             If JugadorRojo.IsChecked = True Then
                 If _objeto.Fill = ChessPieces.AlfilRojo Then
-
+                    MovimientoAlfil()
                 ElseIf _objeto.Fill = ChessPieces.CaballoRojo Then
-
+                    MovimientoCaballo()
                 ElseIf _objeto.Fill = ChessPieces.PeonRojo Then
                     MovimientoPeon()
-                    JugadorRojo.IsChecked = False
-                    JugadorNegro.IsChecked = True
                 ElseIf _objeto.Fill = ChessPieces.ReinaRojo Then
-
+                    MovimientoReina()
                 ElseIf _objeto.Fill = ChessPieces.ReyRojo Then
-
+                    MovimientoRey()
                 ElseIf _objeto.Fill = ChessPieces.TorreRojo Then
-
+                    MovimientoTorre()
                 Else
                     _objeto = Nothing
                 End If
-
             ElseIf JugadorNegro.IsChecked = True Then
                 If _objeto.Fill = ChessPieces.AlfilNegro Then
-
+                    MovimientoAlfil()
                 ElseIf _objeto.Fill = ChessPieces.CaballoNegro Then
-
+                    MovimientoCaballo()
                 ElseIf _objeto.Fill = ChessPieces.PeonNegro Then
                     MovimientoPeon()
-                    JugadorNegro.IsChecked = False
-                    JugadorRojo.IsChecked = True
                 ElseIf _objeto.Fill = ChessPieces.ReinaNegro Then
-
+                    MovimientoReina()
                 ElseIf _objeto.Fill = ChessPieces.ReyNegro Then
-
+                    MovimientoRey()
                 ElseIf _objeto.Fill = ChessPieces.TorreNegro Then
-
+                    MovimientoTorre()
                 Else
                     _objeto = Nothing
                 End If
-
             End If
         End If
-    End Sub
-
-    Private Sub EliminarPieza()
-        Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
     End Sub
 
     Private Sub Mover()
@@ -261,27 +242,64 @@ Class MainWindow
                     If _mouseY - 1 = _newY Or _mouseY - 2 = _newY Then
                         If IsNothing(_matriz(_mouseX, 2)) Then
                             Mover()
+                            JugadorRojo.IsChecked = False
+                            JugadorNegro.IsChecked = True
                         End If
                     End If
                 ElseIf _mouseX <> _newX Or _newY = _mouseY Then ''Error de movimiento
                     _objeto = Nothing
-
-                    MsgBox("No puede mover en ese lugar")
+                    MsgBox("Movimiento Ilegal")
+                    JugadorRojo.IsChecked = True
+                    JugadorNegro.IsChecked = False
                 ElseIf _mouseY - 1 = _newY Then
                     Mover()
+                    JugadorRojo.IsChecked = False
+                    JugadorNegro.IsChecked = True
                 End If
             ElseIf _objeto.Fill = ChessPieces.PeonNegro Then
                 If _newY = 6 Then
                     If _mouseY + 1 = _newY Or _mouseY + 2 = _newY Then
                         If IsNothing(_matriz(_mouseX, 5)) Then
                             Mover()
+                            JugadorNegro.IsChecked = False
+                            JugadorRojo.IsChecked = True
                         End If
                     End If
                 ElseIf _mouseY + 1 = _newY Then
                     Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
                 ElseIf _mouseX <> _newX Or _newY = _mouseY Then ''Error de movimiento
                     _objeto = Nothing
-                    MsgBox("No puede mover en ese lugar")
+                    MsgBox("Movimiento Ilegal")
+                    JugadorNegro.IsChecked = True
+                    JugadorRojo.IsChecked = False
+                End If
+            End If
+        ElseIf (_mouseX = _newX + 1) Or (_mouseX = _newX - 1) Then
+            If JugadorRojo.IsChecked = True Then
+                If (_mouseY = _newY + 1) Then
+                    If Not IsNothing(_matriz(_mouseX, _mouseY)) Then
+                        If _matriz(_mouseX, _mouseY).fill.ToString.Contains("Negro") Then
+                            Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                            _matriz(_mouseX, _mouseY) = Nothing
+                            Mover()
+                            JugadorRojo.IsChecked = False
+                            JugadorNegro.IsChecked = True
+                        End If
+                    End If
+                End If
+            ElseIf JugadorNegro.IsChecked = True Then
+                If (_mouseY = _newY - 1) Then
+                    If Not IsNothing(_matriz(_mouseX, _mouseY)) Then
+                        If _matriz(_mouseX, _mouseY).fill.ToString.Contains("Rojo") Then
+                            Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                            _matriz(_mouseX, _mouseY) = Nothing
+                            Mover()
+                            JugadorNegro.IsChecked = False
+                            JugadorRojo.IsChecked = True
+                        End If
+                    End If
                 End If
             End If
         Else
@@ -290,4 +308,178 @@ Class MainWindow
     End Sub
 
 
+    Private Sub MovimientoTorre()
+        If _newX = _mouseX Or _newY = _mouseY Then
+            If _objeto.Fill = ChessPieces.TorreRojo Then
+                If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                    Mover()
+                    JugadorRojo.IsChecked = False
+                    JugadorNegro.IsChecked = True
+                ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Negro") Then
+                    Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                    _matriz(_mouseX, _mouseY) = Nothing
+                    Mover()
+                    JugadorRojo.IsChecked = False
+                    JugadorNegro.IsChecked = True
+                End If
+            ElseIf _objeto.Fill = ChessPieces.TorreNegro Then
+                If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                    Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
+                ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Rojo") Then
+                    Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                    _matriz(_mouseX, _mouseY) = Nothing
+                    Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
+                End If
+            End If
+        Else
+            _objeto = Nothing
+        End If
+    End Sub
+
+    Private Sub MovimientoAlfil()
+        Dim mov1 As Integer = Math.Abs(_newY - _mouseY)
+        Dim mov2 As Integer = Math.Abs(_newX - _mouseX)
+        If _objeto.Fill = ChessPieces.AlfilRojo Then
+            If mov1 = mov2 Then
+                If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                    Mover()
+                    JugadorRojo.IsChecked = False
+                    JugadorNegro.IsChecked = True
+                ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Negro") Then
+                    Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                    _matriz(_mouseX, _mouseY) = Nothing
+                    Mover()
+                    JugadorRojo.IsChecked = False
+                    JugadorNegro.IsChecked = True
+                End If
+            End If
+        ElseIf _objeto.Fill = ChessPieces.AlfilNegro Then
+
+            If mov1 = mov2 Then
+                If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                    Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
+                ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Rojo") Then
+                    Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                    _matriz(_mouseX, _mouseY) = Nothing
+                    Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
+                End If
+            End If
+        Else
+            _objeto = Nothing
+        End If
+    End Sub
+
+    Public Sub MovimientoRey()
+        If _objeto.Fill = ChessPieces.ReyRojo Then
+            If (_mouseX = _newX + 1) Or (_mouseX = _newX - 1) Or (_mouseX = _newX) Then
+                If (_mouseY = _newY + 1) Or (_mouseY = _newY - 1) Or (_mouseY = _newY) Then
+                    If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                        Mover()
+                        JugadorNegro.IsChecked = True
+                        JugadorRojo.IsChecked = False
+                    ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Negro") Then
+                        Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                        _matriz(_mouseX, _mouseY) = Nothing
+                        Mover()
+                        JugadorNegro.IsChecked = True
+                        JugadorRojo.IsChecked = False
+                    End If
+                End If
+            End If
+        ElseIf _objeto.Fill = ChessPieces.ReyNegro Then
+            If (_mouseX = _newX + 1) Or (_mouseX = _newX - 1) Or (_mouseX = _newX) Then
+                If (_mouseY = _newY + 1) Or (_mouseY = _newY - 1) Or (_mouseY = _newY) Then
+                    If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                        Mover()
+                        JugadorNegro.IsChecked = False
+                        JugadorRojo.IsChecked = True
+                    ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Rojo") Then
+                        Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                        _matriz(_mouseX, _mouseY) = Nothing
+                        Mover()
+                        JugadorNegro.IsChecked = False
+                        JugadorRojo.IsChecked = True
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub MovimientoCaballo()
+        If _objeto.Fill = ChessPieces.CaballoRojo Then
+            If (_mouseX = _newX + 1) Or (_mouseX = _newX - 1) Or (_mouseX = _newX + 2) Or (_mouseX = _newX - 2) Then
+                If (_mouseY = _newY + 2) Or (_mouseY = _newY - 2) Or (_mouseY = _newY + 1) Or (_mouseY = _newY - 1) Then
+                    If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                        Mover()
+                        JugadorNegro.IsChecked = True
+                        JugadorRojo.IsChecked = False
+                    ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Negro") Then
+                        Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                        _matriz(_mouseX, _mouseY) = Nothing
+                        Mover()
+                        JugadorNegro.IsChecked = True
+                        JugadorRojo.IsChecked = False
+                    End If
+                End If
+            End If
+        ElseIf _objeto.Fill = ChessPieces.CaballoNegro Then
+            If (_mouseX = _newX + 1) Or (_mouseX = _newX - 1) Or (_mouseX = _newX + 2) Or (_mouseX = _newX - 2) Then
+                If (_mouseY = _newY + 2) Or (_mouseY = _newY - 2) Or (_mouseY = _newY + 1) Or (_mouseY = _newY - 1) Then
+                    If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                        Mover()
+                        JugadorNegro.IsChecked = False
+                        JugadorRojo.IsChecked = True
+                    ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Rojo") Then
+                        Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                        _matriz(_mouseX, _mouseY) = Nothing
+                        Mover()
+                        JugadorNegro.IsChecked = False
+                        JugadorRojo.IsChecked = True
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub MovimientoReina()
+        Dim mov1 As Integer = Math.Abs(_newY - _mouseY)
+        Dim mov2 As Integer = Math.Abs(_newX - _mouseX)
+        If _objeto.Fill = ChessPieces.ReinaRojo Then
+            If _newX = _mouseX Or _newY = _mouseY Or mov1 = mov2 Then
+                If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                    Mover()
+                    JugadorNegro.IsChecked = True
+                    JugadorRojo.IsChecked = False
+                ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Negro") Then
+                    Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                    _matriz(_mouseX, _mouseY) = Nothing
+                    Mover()
+                    JugadorNegro.IsChecked = True
+                    JugadorRojo.IsChecked = False
+                End If
+            End If
+        ElseIf _objeto.Fill = ChessPieces.ReinaNegro Then
+            If _newX = _mouseX Or _newY = _mouseY Or mov1 = mov2 Then
+                If IsNothing(_matriz(_mouseX, _mouseY)) Then
+                    Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
+                ElseIf _matriz(_mouseX, _mouseY).Fill.ToString.Contains("Rojo") Then
+                    Tablero.Children.Remove(_matriz(_mouseX, _mouseY))
+                    _matriz(_mouseX, _mouseY) = Nothing
+                    Mover()
+                    JugadorNegro.IsChecked = False
+                    JugadorRojo.IsChecked = True
+                End If
+            End If
+        End If
+    End Sub
 End Class
